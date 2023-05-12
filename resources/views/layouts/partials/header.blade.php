1ea5fe552f70e5bc56f886c85d2b34dc4a4b8a3c
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-05-12 10:12:25 +0200

    feat: tiny modification added

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark __navbar">
     <div class="container-fluid">
-      <a class="navbar-brand text-white fs-3" href="#">Trans Table Laravel</a>
+      <a class="navbar-brand text-white fs-3" href="#">Trains Table Laravel</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
